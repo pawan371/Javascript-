4f4cb3e31b6992a0ce93449e48665c5dd35d3deb
Pawan Tiwari <pawantiwari3943@gmail.com>
2026-03-12 23:31:23 +0530

    variable in javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+//Variable ko kaise banate hai 
+
+/*let name="pawan";
+if(true){
+  let c=90;
+}
+console.log(name); 
+console.log(c);//error generated
+
+*//*
+const account=1234;
+
+console.log(account);
+*/
+var a=10;
+var a=30;
+
+console.log(a);
+if
